a583d140801971e65976ef90dbbd21f23f86ccd2
angelODev26 <angel0626199664@gmail.com>
2025-09-30 17:05:23 -0500

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/athletecore/api/AthletecoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.athletecore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthletecoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthletecoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athletecore/api/AthletecoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athletecore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthletecoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
